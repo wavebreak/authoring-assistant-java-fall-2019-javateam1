83b5a76ca65dc01b59a7b3d74b6bde552f5d75e9
Firerat626 <j.c.nelson.b@gmail.com>
2019-10-03 11:35:28 -0500

    beginning blueprint

--- AuthoringAssistant.java
+++ AuthoringAssistant.java
@@ -1,9 +1,18 @@
 import java.util.Scanner;
 
 public class AuthoringAssistant {
-   
+
    public static void main(String[] args) {
-      /* Type your code here. */
-      
+      Scanner scnr = new Scanner(System.in);
+      String userStr;
+      char menuOp = 'o';
+
+      System.out.println("Enter a sample text:");
+      userStr = scnr.nextLine();
+      System.out.println("You entered: " + userStr);
+
+      do{
+         menuOp = ;//FIXME: Make printMenu()
+      }while(menuOp != 'q');
    }
 }
