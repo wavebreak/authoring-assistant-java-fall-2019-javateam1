78df56309dfee1f3b1d88c649720dcc3d6c8775e
Firerat626 <j.c.nelson.b@gmail.com>
2019-10-08 11:13:16 -0500

    for menu w/out validation

--- AuthoringAssistant.java
+++ AuthoringAssistant.java
@@ -5,45 +5,35 @@ public class AuthoringAssistant {
 
    public static char printMenu(){
       char input;
-      while (true){
+       System.out.println("MENU");
+       System.out.println("c - Number of non-whitespace characters");
+       System.out.println("w - Number of words");
+       System.out.println("f - Find text");
+       System.out.println("r - Replace all !'s");
+       System.out.println("s - Shorten spaces");
+       System.out.println("q - Quit");
+       System.out.println();
+       System.out.println("Choose an option: ");
+       input = scnr.next().charAt(0);
 
-         System.out.println("MENU");
-         System.out.println("c - Number of non-whitespace characters");
-         System.out.println("w - Number of words");
-         System.out.println("f - Find text");
-         System.out.println("r - Replace all !'s");
-         System.out.println("s - Shorten spaces");
-         System.out.println("q - Quit");
-         System.out.println();
-         System.out.println("Choose an option: ");
-         input = scnr.next().charAt(0);
+      /* while (input != 'c' || input != 'w' || input != 'f' || input != 'r' || input != 's'){
+           System.out.println("Choose an option: ");
+           input = scnr.next().charAt(0);
+       }*/
 
-
-      }
-
-         /*cout << "MENU" << endl;
-         cout << "c - Number of non-whitespace characters" << endl;
-         cout << "w - Number of words" << endl;
-         cout << "f - Find text" << endl;
-         cout << "r - Replace all !'s" << endl;
-         cout << "s - Shorten spaces" << endl;
-         cout << "q - Quit" << endl;
-         cout << endl << "Choose an option: ";
-         cin >> input;
-         cout << endl;*/
-
-      //return 0;
-
+     return input;
    }
 
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
       String userStr;
+      String findStr;
       char input;
 
       System.out.println("Enter a sample text:");
       userStr = scnr.nextLine();
       System.out.println("\nYou entered: " + userStr);
+      System.out.println();
 
       do{
           input = printMenu();
@@ -59,7 +49,7 @@ public class AuthoringAssistant {
               case 'f':
                   System.out.println("Enter a word or phrase to be found");
                   findStr = scnr.nextLine();
-                  findText(findStr, userStr);
+                 // findText(findStr, userStr);
                   break;
 
               case 'r':
